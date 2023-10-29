805bdaaa2976ab5cfdeec34f197e963cceb4b201
Ivan <igutierrez0794@gmail.com>
2023-10-29 15:37:45 -0400

    first commit

---
+++ user.py
@@ -0,0 +1,17 @@
+class User:
+    def __init__(self, first_name, last_name, email, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
+        self.is_rewards_member = False
+        self.gold_card_points = 0
+
+    def display_info(self):
+        print()
+
+    def enroll(self):
+        pass
+
+    def spend_points(self, amount):
+        pass
